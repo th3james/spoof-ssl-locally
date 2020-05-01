c83386b546c1ddbb85857e38057fd5db38ed2e3e
James Cox-Morton <james.cox-morton@arm.com>
2020-05-01 13:01:26 +0100

    Finish configuration script

--- configure-project.sh
+++ configure-project.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
+echo "What hostname do you want to spoof?"
+
+read spoof_host
+
+echo "What is your target container called?"
+
+read web_app_container
+
+echo "What port is your container running on?"
+
+read web_app_port
+
+echo "What network is your container in?"
+
+read target_network
+
 LC_ALL=C find -XE . \
   -type f -and -regex "\.\/(docker-compose.yml|generate-certs.sh|nginx.conf)" \
-  -exec sed -i '' 's/§hostname_to_spoof§/my-host.lol/g' {} +
+  -exec sed -i '' "s/§hostname_to_spoof§/$spoof_host/g" {} +
+
+LC_ALL=C find -XE . \
+  -type f -and -regex "\.\/(docker-compose.yml|generate-certs.sh|nginx.conf)" \
+  -exec sed -i '' "s/§web_app_container§/$web_app_container/g" {} +
+
+LC_ALL=C find -XE . \
+  -type f -and -regex "\.\/(docker-compose.yml|generate-certs.sh|nginx.conf)" \
+  -exec sed -i '' "s/§web_app_port§/$web_app_port/g" {} +
+
+LC_ALL=C find -XE . \
+  -type f -and -regex "\.\/(docker-compose.yml|generate-certs.sh|nginx.conf)" \
+  -exec sed -i '' "s/§target_network§/$target_network/g" {} +
