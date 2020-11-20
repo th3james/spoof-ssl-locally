244558449740d2a3436cb66e723ded7ad3af151a
James Cox-Morton <th3james@fastmail.fm>
2020-11-20 15:03:59 +0000

    Set AltName correctly

--- generate-certs.sh
+++ generate-certs.sh
@@ -3,6 +3,6 @@ mkdir -p ssl_certs
 openssl req -x509 -out ssl_certs/self-signed-spoof-cert.crt -keyout ssl_certs/self-signed-spoof-cert.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=§hostname_to_spoof§' -extensions EXT -config <( \
-   printf "[dn]\nCN=§hostname_to_spoof§\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+   printf "[dn]\nCN=§hostname_to_spoof§\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:§hostname_to_spoof§\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 echo "Certificates written to ssl_certs/"
